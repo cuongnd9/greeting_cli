651c01e4224e305316a9360acc0e66f17ada92eb
Cuong Duy Nguyen <103cuong@gmail.com>
2020-08-10 16:52:03 +0700

    feat: support linux

--- install_linux.sh
+++ install_linux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # download binary file from GitHub
-curl https://raw.githubusercontent.com/103cuong/greeting_cli/master/greeting_cli --output greeting_cli
+curl https://raw.githubusercontent.com/103cuong/greeting_cli/master/greeting_cli_linux --output greeting_cli
 # move this file to /usr/local/bin
 sudo mv greeting_cli /usr/local/bin
 # set permission for this cli
